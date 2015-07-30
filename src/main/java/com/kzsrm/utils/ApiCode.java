df9d824eac60b164807940aa2bc5b822e16381c9
wangyinzhehappy@163.com <wangyinzhehappy@163.com>
2015-07-30 19:33:28 +0800

    apiCode定义

---
+++ src/main/java/com/kzsrm/utils/ApiCode.java
@@ -0,0 +1,17 @@
+package com.kzsrm.utils;
+
+/**
+ * 返回apiCode值
+ * @author wwy
+ *
+ */
+public class ApiCode {
+	/*
+	 * 参数错误
+	 */
+	public static Integer PARG_ERR = 1001;
+	/*
+	 * 参数错误
+	 */
+	public static Integer NO_DATA = 1002;
+}
